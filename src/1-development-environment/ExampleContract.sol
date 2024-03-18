255bbda707e4681d11f8544fb7acc60cebcc2679
Pere <pere@MacBook-Pro-de-Pere.local>
2024-03-18 16:23:49 -0300

    add exercise 1

---
+++ src/1-development-environment/ExampleContract.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract ExampleContract {
+
+	function helloWorld(uint256 input) public pure returns (uint256) {
+		return input;
+	}
+	
+	function haloDunia() public pure returns (bool) {
+		return true;
+	}
+}
